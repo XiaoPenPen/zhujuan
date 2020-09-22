3f78f66cd8adf8f9e15e65cd7cd55c1338741908
123456 <xu_cpeng@heetian.com>
2020-09-22 14:27:09 +0800

    init

---
+++ src/main/java/com/game/zhujuan/ZhujuanApplication.java
@@ -0,0 +1,13 @@
+package com.game.zhujuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhujuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhujuanApplication.class, args);
+	}
+
+}
